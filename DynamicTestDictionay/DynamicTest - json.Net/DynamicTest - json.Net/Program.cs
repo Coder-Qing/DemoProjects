0f8228b159e731f73c60dc0f4ece8da200be7e9a
MH <977434247@qq.com>
2021-07-04 20:46:29 +0800

    提交json.Net 转化为对象 dynamic调用其动态属性的demo

---
+++ DynamicTestDictionay/DynamicTest - json.Net/DynamicTest - json.Net/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTest___json.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"
+            {
+
+                'name':'Jon Skeet',
+                'address':{
+                    'town':'Reading',
+                    'country':'UK'
+                }
+            }".Replace('\'','"');
+
+            //静态类型视图
+            JObject obj1 = JObject.Parse(json);
+
+            Console.WriteLine(obj1["address"]["town"]);
+
+            //动态类型视图
+            dynamic obj2 = obj1;
+            Console.WriteLine(obj2.address.town);
+
+            Console.ReadKey();
+        }
+    }
+}
