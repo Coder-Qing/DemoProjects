fc27e759ef101a81fea19d3481790357f1e6b900
狄青 <977434247@qq.com>
2021-10-25 21:37:20 +0800

    暂存

---
+++ Console_20210808/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_20210808
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dynamic function =(Func<dynamic,dynamic>) (x => x * 2);
+            //Console.WriteLine(function(0.75));
+
+            Predicate<int> predicate = (x => x < 15);
+
+            List<int> list = new List<int>();
+            IList list2 = list;
+            Console.WriteLine(list2.IsFixedSize);
+
+
+            dynamic souce = new List<dynamic>
+            {
+                5,
+                2.75,
+                TimeSpan.FromSeconds(45)
+            };
+            
+            new List<int>().Find(predicate);
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DynamicTestDictionay/DynamicTest - DynamicObject/DynamicTest - DynamicObject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTest___DynamicObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic example = new SimpleDynamicExample();
+            example.CallSomeMethod("x",10);
+            Console.WriteLine(example.SomeProperty);
+
+            Console.ReadKey();
+        }
+
+        class SimpleDynamicExample:DynamicObject
+        {
+            //调用方法  args是参数
+            public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+            {
+                Console.WriteLine("Invoked: {0}{1}",binder.Name,string.Join(", ",args));
+                result = null;
+                return true;
+            }
+
+            //获取属性
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                result = "Fetched: " + binder.Name;
+                return true;
+            }
+        }
+    }
+}
